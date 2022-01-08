39eb97578e178ae935e0013ecac837f2476e8ced
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-01-09 00:05:14 +0900

    add 14.Longest Common Prefix

--- 14.longest-common-prefix.php
+++ 14.longest-common-prefix.php
@@ -12,80 +12,22 @@ class Solution {
      * @return String
      */
     function longestCommonPrefix($strs) {
-        if (count($strs) === 1) {
-            return $strs[0];
-        }
-
-        $first = str_split($strs[0]);
-        $second = str_split($strs[1]);
-        $result = "";
-
-        if (count($strs) === 2) {
-            if ($first === $second) {
-                return $strs[0];
-            }
-            for ($i = 0; $i < count($first); $i++) {
-                if (!($first[$i] === $second[$i])) {
-                    if ($i >= 0) {
-                        $result = substr($strs[0],0,$i);
-                    }
-                    break;
-                }
-                if (count($first) === $i+1) {
-                    $result = substr($strs[0],0,$i+1);
-                    break;
-                }
-            }
-            return $result;
-        }
-            
-
-        $third = str_split($strs[2]);
-
-        if (count($strs) === 3) {
-            if (($first === $second) && ($first === $third)) {
-                return $strs[0];
-            }
-            for ($i = 0; $i < count($first); $i++) {
-                if (!(
-                    ($first[$i] === $second[$i]) &&
-                    ($first[$i] === $third[$i])
-                    ))
-                {
-                    if ($i >= 0) {
-                        $result = substr($strs[0],0,$i);
-                    }
-                    break;
-                }
-            }
+        $result = '';
+        if (count($strs) === 0) {
             return $result;
         }
 
-        $forth = str_split($strs[3]);
-
-        if (count($strs) === 4) {
-            if (
-                ($first === $second) && ($first === $third) &&
-                ($first === $forth)
-                ) {
-                return $strs[0];
-            }
-            for ($i = 0; $i < count($first); $i++) {
-                if (!(
-                    ($first[$i] === $second[$i]) &&
-                    ($first[$i] === $third[$i]) &&
-                    ($first[$i] === $forth[$i])
-                    ))
-                {
-                    if ($i >= 0) {
-                        $result = substr($strs[0],0,$i);
-                    }
-                    break;
+        for ($i = 0; $i < count(str_split($strs[0])); $i++) {
+            $char = substr($strs[0],$i,1);
+            var_dump($char);
+            for ($j = 0;$j < count($strs); $j++) {
+                if ($strs[$j][$i] !== $char) {
+                    return $result;
                 }
             }
-            return $result;
+            $result = $result . $char;
         }
-        
+        return $result;
     }
 }
 // @lc code=end
