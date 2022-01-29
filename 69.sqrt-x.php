17c435f51f01e94a81b50d75f2c0513e7501fd23
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-01-30 00:24:04 +0900

    [69] Sqrt(x)

---
+++ 69.sqrt-x.php
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=69 lang=php
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x) {
+        return floor(sqrt($x));
+    }
+}
+// @lc code=end
+
