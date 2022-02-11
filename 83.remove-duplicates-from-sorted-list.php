6617da531b736a5bd26add8ba9febf607be5a058
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-02-12 02:34:46 +0900

    [83] Remove Duplicates from Sorted List

--- 83.remove-duplicates-from-sorted-list.php
+++ 83.remove-duplicates-from-sorted-list.php
@@ -23,8 +23,36 @@ class Solution {
      * @return ListNode
      */
     function deleteDuplicates($head) {
+        if ($head == null) {
+            return null;
+        }
+        $pre = $head;
+        while ($pre!= null) {
+            $next = $pre->next;
+            if ($next->val === $pre->val) {
+                $pre->next = $pre->next->next;
+            }else{
+                $pre = $pre->next;
+            }
+        }
+
+        return $head;
+
+        // $before_num = 0;
+        // return array_reduce($head,function($result_array, $current_num) {
+        //     if ($current_num !== $before_num) {
+        //         $result_array[] = $current_num;
+        //     }
+        //     $before_num = $current_num;
+        //     return $result_array;
+        // },[]);
+    }
+
+
+    function getData($carryObject) {
         $before_num = 0;
-        return array_reduce($head,function($result_array, $current_num) {
+        return array_reduce($carryObject,function($result_array, $current_num) {
+            var_dump($current_num);
             if ($current_num !== $before_num) {
                 $result_array[] = $current_num;
             }
