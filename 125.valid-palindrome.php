07956cf21d23f5ee8aca32b82ab32b263038e22e
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-03-05 23:54:10 +0900

    [125] Valid Palindrome

---
+++ 125.valid-palindrome.php
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=125 lang=php
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isPalindrome($s) {
+        
+    }
+}
+// @lc code=end
+
